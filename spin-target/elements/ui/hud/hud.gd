09717fd5a443bcb9802a6ffb6dc1735bd7f12bf4
Marsel <marselshkl2006-arch>
2026-03-12 22:44:51 +0600

    Добавил новый счетчик

--- spin-target/elements/ui/hud/hud.gd
+++ spin-target/elements/ui/hud/hud.gd
@@ -1,14 +1,18 @@
 extends CanvasLayer
 
 @onready var apples_label := $MarginContainer/VBoxContainer/HBoxContainer/HBoxContainer2/Label
-@onready var knives_label := $MarginContainer/VBoxContainer/HBoxContainer/HBoxContainer3/KnivesLabel
+@onready var knives_row := $MarginContainer/VBoxContainer/HBoxContainer2/VBoxContainer
 
 
 func _ready() -> void:
 	Events.apples_changed.connect(_on_apples_changed)
 	_on_apples_changed(Globals.apples)
-	_update_knives_label()
 	_hide_home_on_start()
+	_update_knives_visual()
+
+
+func _process(_delta: float) -> void:
+	_update_knives_visual()
 
 
 func _on_apples_changed(apples: int) -> void:
@@ -20,18 +24,34 @@ func _on_home_button_pressed() -> void:
 	Events.location_changed.emit(Events.LOCATIONS.START)
 
 
-func _process(_delta: float) -> void:
-	_update_knives_label()
-
-
-func _update_knives_label() -> void:
-	if knives_label and is_instance_valid(get_tree().get_first_node_in_group("target")):
-		var shooter := get_tree().get_first_node_in_group("knifeshooter")
-		if shooter:
-			knives_label.text = str(shooter.remaining_knives + 1)
-
-
 func _hide_home_on_start() -> void:
 	var home_button := $MarginContainer/VBoxContainer/HBoxContainer/HomeButton
 	if home_button and get_tree().current_scene and get_tree().current_scene.name == "StartScreen":
 		home_button.hide()
+
+
+func _update_knives_visual() -> void:
+	if not knives_row:
+		return
+
+	var current_scene := get_tree().current_scene
+	var is_game := current_scene and current_scene.name == "Game"
+
+	# показываем визуальный счётчик только в игре
+	knives_row.get_parent().visible = is_game
+	if not is_game:
+		return
+
+	var shooter := get_tree().get_first_node_in_group("knifeshooter")
+	if not shooter:
+		return
+
+	var throws_left: int = shooter.remaining_knives + 1
+	var icons := knives_row.get_children()
+
+	for i in range(icons.size()):
+		var icon = icons[i]
+		if i < throws_left:
+			icon.modulate = Color(1, 1, 1, 1)
+		else:
+			icon.modulate = Color(1, 1, 1, 0.2)
